ccfd2b527f89a72fcec61a96882ce20bc33b3cf7
drighetto <drighetto@excellium-services.com>
2020-11-28 17:22:12 +0100

    Add a new api prefix and optimize the script to extract API endpoints.

--- extract-web-api-endpoints.py
+++ extract-web-api-endpoints.py
@@ -8,12 +8,8 @@ Filters needed via matching of the case insensitive regex for the header Content
 # Match all URL pattern starting with ' or " and followed by a /
 # like '/api/v1/slo.ts/boo-ked/aaa_bbb'
 expr = r'[\'"]\/[\d\w\/\-\.]+[\'"]'
-# Extract the whole response body
-script = ""
-for line in sys.stdin:
-    if len(line.strip("\r").strip("\n").strip(" ")) == 0:
-        continue
-    script += line
+# Read the whole response body
+script = "".join(sys.stdin)
 # Extract the API endpoints via the regex and remove duplicates
 endpoints = re.findall(expr, script, re.IGNORECASE|re.MULTILINE)
 result = list(dict.fromkeys(endpoints))

fa22a06a1ccd1eebadf7022b980f7cddedf9a9f3
drighetto <drighetto@excellium-services.com>
2020-11-29 11:05:29 +0100

    Enhance the extraction regex in order to do not miss endpoints not starting with a slash.

--- extract-web-api-endpoints.py
+++ extract-web-api-endpoints.py
@@ -5,11 +5,16 @@ Target tool: Message viewers
 Require that HTTP headers NOT been passed
 Filters needed via matching of the case insensitive regex for the header Content-Type: [a-z]+\/(javascript|x\-javascript)
 """
-# Match all URL pattern starting with ' or " and followed by a /
-# like '/api/v1/slo.ts/boo-ked/aaa_bbb'
-expr = r'[\'"]\/[\d\w\/\-\.]+[\'"]'
-# Read the whole response body
-script = "".join(sys.stdin)
+# Match all URL patterns starting with ' or " and optionally followed by a /
+# like '/api/v1/slo-ts/bo2o_ked/a6aa_bbb'
+# like 'api/v1/slo-ts/bo2o_ked/a6aa_bbb'
+expr = r'[\'"]\/?[\d\w_\-]+\/[\d\w_\-\/]+[\'"]'
+# Extract the whole response body
+script = ""
+for line in sys.stdin:
+    if len(line.strip("\r").strip("\n").strip(" ")) == 0:
+        continue
+    script += line
 # Extract the API endpoints via the regex and remove duplicates
 endpoints = re.findall(expr, script, re.IGNORECASE|re.MULTILINE)
 result = list(dict.fromkeys(endpoints))

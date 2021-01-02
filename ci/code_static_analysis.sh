fdb05081fbd2fc1baf1faea304a072634f286e18
drighetto <drighetto@excellium-services.com>
2021-01-02 18:59:36 +0100

    Add UT using a tests matrix

--- ci/code_static_analysis.sh
+++ ci/code_static_analysis.sh
@@ -7,6 +7,10 @@
 cdir=$(pwd)
 echo "[+] Current folder: $cdir"
 exit_rc=0
+echo "[+] Run unit test suites..."
+pytest
+rc=$?
+exit_rc=$((exit_rc+rc))
 echo "[+] Extract 'flake8' and 'bandit' configuration from VSCode workspace file..."
 flake8Args=$(cat project.code-workspace | jq -r '.settings["python.linting.flake8Args"]|join(" ")')
 banditArgs=$(cat project.code-workspace | jq -r '.settings["python.linting.banditArgs"]|join(" ")')
@@ -17,7 +21,9 @@ python -m flake8 $flake8Args $cdir
 rc=$?
 exit_rc=$((exit_rc+rc))
 echo "[+] Run bandit..."
-python -m bandit $banditArgs $cdir 
+# The "exclude" option do not work correctly if a relative path is specified
+# See https://github.com/PyCQA/bandit/issues/488
+python -m bandit $banditArgs --exclude $cdir/tests $cdir 
 rc=$?
 exit_rc=$((exit_rc+rc))
 if [ $exit_rc -ne 0 ]
---
+++ tests/test_scripts.py
@@ -0,0 +1,53 @@
+import json
+import subprocess
+"""
+Test suites in charge of testing all the scripts based on the expectation defined in file "test_scripts.json"
+for the 2 test cases by scripts:
+    - The one in which data must be found where the content of the file "[script_name_without_extension]_findcontent.txt" is used as input source
+    - The one in which no data must be found where the content of the file "[script_name_without_extension]_findnothing.txt" is used as input source
+"""
+
+
+def __get_property(script_name_without_extension, test_case):
+    with open("tests/test_scripts.json", "r") as cfg:
+        config = json.load(cfg)
+    return config[script_name_without_extension][test_case]
+
+
+def __get_scripts():
+    with open("tests/test_scripts.json", "r") as cfg:
+        config = json.load(cfg)
+    return config.keys()
+
+
+def __run_test(test_data_file_name, script_name_without_extension):
+    p = subprocess.Popen(["python", f"{script_name_without_extension}.py"], stdin=open(f"tests/{test_data_file_name}"), stdout=subprocess.PIPE)
+    p.wait()
+    return (p.returncode, p.stdout.read().decode("utf-8"))
+
+
+def __check_result(props, rc, content):
+    if props["type"] == "Commentators" or props["type"] == "Message viewers":
+        marker = props["marker"]
+        expected_rc = props["expected-rc"]
+        assert rc == expected_rc, f"RC is not equals to {expected_rc} (RC: {rc})"
+        assert marker in content, f"Marker '{marker}' not found in '{content}'"
+    elif props["type"] == "Highlighters":
+        expected_rc = props["expected-rc"]
+        assert rc == expected_rc, f"RC is not equals to {expected_rc} (RC: {rc})"
+
+
+def test_scripts():
+    # Get the list of script to tests
+    scripts = __get_scripts()
+    # Apply test to each of them
+    for script in scripts:
+        # Apply the 2 test cases
+        for test_case in ["findcontent", "findnothing"]:
+            print(f"[+] Testing script '{script}' for test case '{test_case}'...")
+            # Get test case properties
+            props = __get_property(script, test_case)
+            # Execute the test case
+            rc, content = __run_test(f"{script}_{test_case}.txt", script)
+            # Verify the execution result
+            __check_result(props, rc, content)
